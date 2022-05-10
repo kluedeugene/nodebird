09a5df36d4693b7553d8c931c6d45d5d9ebdc614
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-05-10 16:37:10 +0900

    node bird first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config(); //dotenv는 최대한 위에 해야 밑에부터적용된다.
+const pageRouter = require('./routes/page');
+
+const app = express();
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+	express: app,
+	watch: true
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+	session({
+		resave: false,
+		saveUninitialized: false,
+		secret: process.env.COOKIE_SECRET,
+		cookie: {
+			httpOnly: true,
+			secure: false
+		}
+	})
+);
+
+app.use('/', pageRouter);
+
+app.use((req, res, next) => {
+	const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+	error.status = 404;
+	next(error);
+});
+
+app.use((err, req, res, next) => {
+	//에러처리 미들웨어 next 필수작성
+	res.locals.message = err.message;
+	res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; //개발모드일시 에러메시지 보여주고, 배포일시는 안보여줌
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+	console.log(app.get('port'), '번 포트에서 대기중');
+});
