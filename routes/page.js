fca73ce635b546a7b19184a6a73fe8193486f927
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-05-14 00:05:50 +0900

    이미지,게시글 출력

--- routes/page.js
+++ routes/page.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const { Post, User } = require('../models');
 
 const router = express.Router();
+
+router.use((req, res, next) => {
+	//미들웨어의 특성을 이용한 변수중복 제거
+	res.locals.user = req.user;
+	res.locals.followCount = 0;
+	res.locals.followerCount = 0;
+	res.locals.followerIdList = [];
+	next();
+});
 
 router.get('/profile', (req, res) => {
 	res.render('profile', { title: '내 정보 -NodeBird' });
@@ -10,12 +20,24 @@ router.get('/join', (req, res) => {
 	res.render('join', { title: '회원가입 -NodeBird' });
 });
 
-router.get('/', (req, res, next) => {
-	const twits = [];
-	res.render('main', {
-		title: 'NodeBird',
-		twits,
-		user: req.user
-	});
+router.get('/', async (req, res, next) => {
+	try {
+		const posts = await Post.findAll({
+			//업로드한 게시물을 찾고
+			include: {
+				model: User,
+				attributes: ['id', 'nick']
+			},
+			order: [['createdAt', 'DESC']]
+		});
+		res.render('main', {
+			title: 'NodeBird',
+			twits: posts //찾은 게시글을 twits로 넣어줌
+			// user: req.user		//res.locals로 빼줄수있다.
+		});
+	} catch (err) {
+		console.log(err);
+		return next(err);
+	}
 });
 module.exports = router;
